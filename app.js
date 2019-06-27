9b71c0ab6c1a659fa886afbe634719ec4445d713
myungho <skqoaudgh2@hanmail.net>
2019-06-27 18:39:05 +0900

    mysql 로컬 서버와 연동

--- app.js
+++ app.js
@@ -1,10 +1,22 @@
 const express = require('express');
+const mysql = require('mysql');
+const dbconfig = require('./config/database.js');
+const connection = mysql.createConnection(dbconfig);
 
 const indexRouter = require('./router/index');
 
 const app = express();
 
 app.use('/', indexRouter);
+
+connection.connect();
+connection.query('SELECT * from employee', function(err, rows, fields) {
+    if (!err)
+        console.log('The solution is: ', rows);
+    else
+        console.log('Error while performing Query.', err);
+});
+connection.end();
 
 app.listen(3000, () => {
     console.log('express server is opened on port 3000');
---
+++ config/database.js
@@ -0,0 +1,7 @@
+module.exports = {
+    host     : 'localhost',
+    user     : 'root',
+    password : 'asd123',
+    port     : 3306,
+    database : 'my_restapi_db'
+}
